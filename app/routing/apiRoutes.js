cfc8a127b0864ff7d5c244613b7e69cbc8b7a204
Brendan Burns <btburns10@gmail.com>
2018-09-03 14:12:45 -0400

    api-route logic complete

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,19 +7,31 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     var userArr = req.body;
-
-    console.log(JSON.stringify(userArr))
+    console.log(JSON.stringify(userArr) + " survey results sent to API!");
     
+    //set variables
     var diffArr = [];
+    var minScoreDiff;
+    var indexOfMatch;
     var totalDiffSum = 0;
+
+    //compatible match logic
     for(var i = 0; i < friends.length; i++) {
       totalDiffSum = 0;
       for(var k = 0; k < (friends[i].scores).length; k++) {
         totalDiffSum += Math.abs(userArr.scores[k] - friends[i].scores[k]);
-          diffArr.push(totalDiffSum);
       }
+      diffArr.push(totalDiffSum);
     }
-    console.log(diffArr);
-    res.json(friends[0]);
+    
+    //response with match from friends api
+    minScoreDiff = Array.min(diffArr);
+    indexOfMatch = diffArr.indexOf(minScoreDiff);
+    res.json(friends[indexOfMatch]);
   })
 }
+
+//array prototype function to find smallest number in array
+Array.min = function(array) {
+  return Math.min.apply(Math, array);
+};
